afb53500ee9b8fce297440555e3d1b16fb1fd5df
Adash Gurung <adashleo@outlook.com>
2021-04-23 00:01:39 +0100

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,9 +1,9 @@
 // https://developers.google.com/custom-search/v1/using_rest
 
-export const API_KEY = "AIzaSyCEwKEIvaSpzw7bjwn9JycLjMkG3ldH_yk";
-//AIzaSyDWiC2HDXUbp7nCQLKSyV-s8rR4KCHGEU8
+export const API_KEY = "api_key";
 
-//AIzaSyBPByqGpkronEgomEXbarnfYVX1OSQKf7w
+
+
 // https://cse.google.com/cse/create/new
 export const CONTEXT_KEY = "525a00a95292b8338";
 
